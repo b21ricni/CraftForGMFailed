30c61dab3479c895c8047ae467fb7fdc59d0e2d0
b21ricni <b21ricni@student.his.se>
2023-07-31 16:12:41 +0200

    Added form
    +newCharacterForm
    +CharacterName
    
    Adding other components when name works properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react"
+import "./styles.css"
+
 export default function App() {
-  return "hi"
+  const [newCharacter, setNewCharacter] = useState("")
+  
+  return (
+    <>
+      <form action="" className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">Character name</label>
+          <input 
+            value={newCharacter} 
+            onChange={e => setNewCharacter(e.target.value)} 
+            type="text" 
+            id="item" 
+          />
+        </div>
+        <button className="btn">Create character</button>
+      </form>
+      <h1 className="header">Characters</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button className="btn btn-danger">X</button>
+        </li>
+      </ul>
+    </>
+  )
 }
